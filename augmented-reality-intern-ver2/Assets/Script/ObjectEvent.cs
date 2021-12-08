b06993add5b477514ea22e2a889e4a91b0d816e4
Tyo Hanan <60575547+tyohanan@users.noreply.github.com>
2021-12-08 20:08:13 +0700

    adding music when detecting image

--- augmented-reality-intern-ver2/Assets/Script/ObjectEvent.cs
+++ augmented-reality-intern-ver2/Assets/Script/ObjectEvent.cs
@@ -7,12 +7,16 @@ public class ObjectEvent : MonoBehaviour
 {
     public GameObject arutalaLogo;
     public GameObject particleStart;
+    public AudioSource bgmGame;
+
     public Vector3 TorqueObject = new Vector3 (0,0,0);
+
     private Rigidbody rbArutalaLogo;
-    private bool ObjectDetected;
+
 
     void Start(){
         rbArutalaLogo = arutalaLogo.GetComponent<Rigidbody>();
+
     }
 
     void Update(){
@@ -23,10 +27,10 @@ public class ObjectEvent : MonoBehaviour
     public void targetDetected(){
         rbArutalaLogo.AddTorque(TorqueObject, ForceMode.Impulse);
         Instantiate(particleStart, transform.position, Quaternion.identity);
+        bgmGame.Play();
     }
 
     public void targetNotDetected(){
-        arutalaLogo.transform.rotation = Quaternion.Euler(0,0,0);
-
+        bgmGame.Stop();
     }
 }
