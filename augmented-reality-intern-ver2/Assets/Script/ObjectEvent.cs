f67c003e5562cc1b78a1b7202b4a9d92076efbb1
Tyo Hanan <60575547+tyohanan@users.noreply.github.com>
2021-12-09 09:04:34 +0700

    fixing particle instantiate

--- augmented-reality-intern-ver2/Assets/Script/ObjectEvent.cs
+++ augmented-reality-intern-ver2/Assets/Script/ObjectEvent.cs
@@ -6,13 +6,14 @@ using Vuforia;
 public class ObjectEvent : MonoBehaviour
 {
     public GameObject arutalaLogo;
-    public GameObject particleStart;
+    public GameObject particleObject;
+    public GameObject particlePosition;
     public AudioSource bgmGame;
 
     public Vector3 TorqueObject = new Vector3 (0,0,0);
     private Rigidbody rbArutalaLogo;
     private Animator animLogo;
-
+    private bool imageDetected;
 
     void Start(){
         rbArutalaLogo = arutalaLogo.GetComponent<Rigidbody>();
@@ -23,16 +24,24 @@ public class ObjectEvent : MonoBehaviour
     void Update(){
         // print(transform.position);
         print (arutalaLogo.transform.rotation);
+        if (imageDetected || Input.GetKeyDown(KeyCode.C)){
+            Instantiate(particleObject, transform.position, particlePosition.transform.rotation);
+            // particleObject.transform.position = transform.position;
+            // particleObject.transform.rotation = particlePosition.transform.rotation;
+            imageDetected = false;
+        }
+
     }
 
     public void targetDetected(){
-        Instantiate(particleStart, transform.position, Quaternion.identity);
+        imageDetected = true;
         bgmGame.Play();
         animLogo.SetBool("onStart", true);
         arutalaLogo.transform.Rotate(0,360,0);
     }
 
     public void targetNotDetected(){
+        imageDetected = false;
         bgmGame.Stop();
         animLogo.SetBool("onStart", false);
     }
